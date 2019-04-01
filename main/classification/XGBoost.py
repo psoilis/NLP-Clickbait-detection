65db2f950b2ef7873d0dea407b00290635e79ffd
agamvrinos <agg_gam@hotmail.com>
2019-04-01 17:11:03 +0200

    Add Classifiers templates

---
+++ main/classification/XGBoost.py
@@ -0,0 +1,10 @@
+class XGBoost:
+
+    def __init__(self):
+        print("Naive Bayes initialized")
+
+    def train(self):
+        print("training")
+
+    def predict(self):
+        print("predict")
